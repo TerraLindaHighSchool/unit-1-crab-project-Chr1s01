8c4216015c9dc08f40aa5d0e8702009e1face3b3
Chr1s01 <89489807+Chr1s01@users.noreply.github.com>
2021-08-30 17:40:04 -0500

    Summary Updated
    Updated 3:39pm

--- Crab.java
+++ Crab.java
@@ -2,13 +2,39 @@ import greenfoot.*;
 
 /**
  * This class defines a crab. Crabs live on the beach.
+ *@author Chr1s01
+ *@version 8/25/2020
+ 
  */
 public class Crab extends Actor
 {
     public void act()
     {
-        // Add your action code here
+        //  move(3);
+         turnAtEdge();
+         checkKeyPress();
+         onCollision();
     }
+
+
+    // Moves the crab
+    
+    // Turns the crab at the edge
+    private void turnAtEdge()
+    {
+        
+    }
+    
+    // Checks for user key presses so user can turn the Crab
+    private void checkKeyPress ( )
+    {
+        
+    }
+    
+    // Check for collisions with other objects
+    private void onCollision ( )
+    {
+        
+    }
+
 }
-
-
--- CrabWorld.java
+++ CrabWorld.java
@@ -11,6 +11,7 @@ public class CrabWorld extends World
         super(560, 560, 1);
         prepare();
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -19,5 +20,21 @@ public class CrabWorld extends World
     {
         Crab crab = new Crab();
         addObject(crab,219,212);
+        Worm worm = new Worm();
+        addObject(worm,61,62);
+        Worm worm2 = new Worm();
+        addObject(worm2,273,67);
+        Worm worm3 = new Worm();
+        addObject(worm3,428,86);
+        Worm worm4 = new Worm();
+        addObject(worm4,78,201);
+        Worm worm5 = new Worm();
+        addObject(worm5,444,237);
+        Worm worm6 = new Worm();
+        addObject(worm6,217,391);
+        Worm worm7 = new Worm();
+        addObject(worm7,472,348);
+        Worm worm8 = new Worm();
+        addObject(worm8,334,200);
     }
 }
