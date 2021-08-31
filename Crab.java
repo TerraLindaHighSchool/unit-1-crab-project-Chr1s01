33b57545a163460dc628dfb42d23c7c70fc5c9c1
Chr1s01 <89489807+Chr1s01@users.noreply.github.com>
2021-08-31 16:57:57 -0500

    Update Crab.java
    updated up to 1.3.3 video

--- Crab.java
+++ Crab.java
@@ -10,7 +10,7 @@ public class Crab extends Actor
 {
     public void act()
     {
-        //  move(3);
+        //  move();
          turnAtEdge();
          checkKeyPress();
          onCollision();
@@ -28,12 +28,26 @@ public class Crab extends Actor
     // Checks for user key presses so user can turn the Crab
     private void checkKeyPress ( )
     {
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(30);
+        }
+        
+        if(Greenfoot.isKeyDown("left"))
+        {
+            turn(-4);
+        }
         
     }
     
     // Check for collisions with other objects
     private void onCollision ( )
     {
+        if(isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+            Greenfoot.playSound("slurp.wav");
+        }
         
     }
 
